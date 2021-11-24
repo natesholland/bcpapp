bcdb68374cad3313decef07180d994afe0014344
Nate Holland <natesholland@gmail.com>
2021-11-24 11:35:08 -0600

    flipping over to fresh rebuild

--- android/app/src/debug/java/com/bcpappfresh/ReactNativeFlipper.java
+++ android/app/src/debug/java/com/bcpappfresh/ReactNativeFlipper.java
@@ -4,7 +4,7 @@
  * <p>This source code is licensed under the MIT license found in the LICENSE file in the root
  * directory of this source tree.
  */
-package com.bcpapp;
+package com.bcpappfresh;
 
 import android.content.Context;
 import com.facebook.flipper.android.AndroidFlipperClient;
--- android/app/src/main/java/com/bcpappfresh/MainActivity.java
+++ android/app/src/main/java/com/bcpappfresh/MainActivity.java
@@ -1,4 +1,4 @@
-package com.bcpapp;
+package com.bcpappfresh;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "bcpapp";
+    return "bcpappfresh";
   }
 }
